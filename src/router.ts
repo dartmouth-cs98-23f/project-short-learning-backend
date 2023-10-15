7e1469692127c136d76b5bca1c225d18e31c5104
siavava <amittaijoel@outlook.com>
2023-10-14 23:43:10 -0400

    add relationship and secondary-user routes

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { requireAuth, requireSignin } from './services/passport';
 import * as User from './controllers/user_controllers';
+import * as Relationships from './controllers/relationship_controllers';
 
 const router = Router();
 
@@ -21,6 +22,90 @@ router.post('/auth/signin', requireSignin, async (req, res) => {
   try {
     const token = User.signin(req.user);
     res.json({ token });
+  } catch (error) {
+    res.status(422).send({ error: error.toString() });
+  }
+});
+
+/***
+ * Get a user's profile (by ID)
+ * 
+ * Useful for getting a user's profile
+ * for display to other users.
+ * 
+ * Does not return sensitive information,
+ * only returns:
+ * - firstName
+ * - lastName
+ * - id
+ * - (maybe eventually) profilePictureURL
+ */
+router.get('/users/:id', requireAuth, async (req, res) => {
+  try {
+    const user = await User.getUser(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(422).send({ error: error.toString() });
+  }
+});
+
+router.get('/relationships/all/:userId', requireAuth, async (req, res) => {
+  try {
+    const relationships = await Relationships.getAll(req.params.userId);
+    res.json(relationships);
+  } catch (error) {
+    res.status(422).send({ error: error.toString() });
+  }
+});
+
+/**
+ * Get outgoing relationships for a user
+ * 
+ * Useful for getting a list of users that a user has sent a friend request to.
+ */
+router.get('/relationships/outgoing/:userId', requireAuth, async (req, res) => {
+  try {
+    const relationships = await Relationships.getOutgoing(req.params.userId);
+    res.json(relationships);
+  } catch (error) {
+    res.status(422).send({ error: error.toString() });
+  }
+});
+
+/**
+ * Get incoming relationships for a user.
+ * 
+ * Useful for getting a list of users that have sent a friend request to a user.
+ */
+router.get('/relationships/incoming/:userId', requireAuth, async (req, res) => {
+  try {
+    const relationships = await Relationships.getIncoming(req.params.userId);
+    res.json(relationships);
+  } catch (error) {
+    res.status(422).send({ error: error.toString() });
+  }
+});
+
+/**
+ * Get pending relationships for a user.
+ * 
+ * Useful for getting a list of users that:
+ *  - have sent a friend request to a user
+ *  - a user has sent a friend request to
+ * ... but the request has not been accepted yet.
+ * 
+ * @param direction - "outgoing" | "incoming" | "all"
+ */
+router.get('/relationships/pending/:userId/:direction', requireAuth, async (req, res) => {
+  try {
+    // get direction from req.params.direction and make sure it is valid
+    const direction = req.params.direction.toLowerCase();
+    if (direction !== 'outgoing' && direction !== 'incoming' && direction !== 'all') {
+      throw new Error('Invalid direction');
+    }
+    
+    const relationships = await Relationships.getPending(req.params.userId, direction);
+    res.json(relationships);
   } catch (error) {
     res.status(422).send({ error: error.toString() });
   }
