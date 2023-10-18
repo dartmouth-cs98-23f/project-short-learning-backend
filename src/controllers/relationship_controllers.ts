24fb22e4b6d85d5dfdb13afa5fcdff2d4c83e847
siavava <amittaijoel@outlook.com>
2023-10-18 07:19:33 -0400

    checkpoint, need clean-up

--- src/controllers/relationship_controllers.ts
+++ src/controllers/relationship_controllers.ts
@@ -51,14 +51,28 @@ export const update = async ({
  * 
  * Useful for getting a list of all users that a user is friends with.
  */
-export const getAll = async (userID: String) => {
+export const getAll = async (userID: string, status?: string) => {
   try {
-    const relationships = await Relationship.find({
-      $or: [
-        { fromUserID: userID },
-        { toUserID: userID },
-      ]
-    });
+    let relationships;
+    if (status) {
+      relationships = await Relationship.find({
+        $and: [
+          { $or: [
+            { fromUserID: userID },
+            { toUserID: userID },
+          ]},
+          { status },
+        ]
+      });
+    }
+    else {
+      relationships = await Relationship.find({
+        $or: [
+          { fromUserID: userID },
+          { toUserID: userID },
+        ]
+      });
+    }
     return relationships;
   } catch (error) {
     return error;
@@ -73,9 +87,15 @@ export const getAll = async (userID: String) => {
  * NOTE: To get **pending** outgoing relationships, use `getPending` instead
  * and specify the relevant `direction = "outgoing"`.
  */
-export const getOutgoing = async (userID: String) => {
+export const getOutgoing = async (userID: string, status?: string) => {
   try {
-    const relationships = await Relationship.find({ fromUserID: userID });
+    let relationships;
+    if (status) {
+      relationships = await Relationship.find({ fromUserID: userID, status });
+    }
+    else {
+      relationships = await Relationship.find({ fromUserID: userID });
+    }
     return relationships;
   } catch (error) {
     return error;
@@ -90,92 +110,98 @@ export const getOutgoing = async (userID: String) => {
  * NOTE: To get **pending** incoming relationships, use `getPending` instead
  * and specify the relevant `direction = incoming`.
  */
-export const getIncoming = async (userID: String) => {
+export const getIncoming = async (userID: string, status? : string) => {
   try {
-    const relationships = await Relationship.find({ toUserID: userID });
+    let relationships;
+    if (status) {
+      relationships = await Relationship.find({ toUserID: userID, status });
+    }
+    else {
+      relationships = await Relationship.find({ toUserID: userID });
+    }
     return relationships;
   } catch (error) {
     return error;
   }
 }
 
-/**
- * Get pending relationships for a user.
- * 
- * Useful for getting a list of users that:
- * - have sent a friend request to a user
- * - a user has sent a friend request to
- * ... but the request has not been accepted yet.
- */
-export const getPending = async (userID: String, direction: "outgoing" | "incoming" | "all") => {
-  try {
-    let relationships;
-    switch (direction) {
-      case "outgoing":
-        relationships = await Relationship.find({ fromUserID: userID, status: "pending" });
-        return relationships;
-      case "incoming":
-        relationships = await Relationship.find({ toUserID: userID, status: "pending" });
-        return relationships;
-      case "all":
-        relationships = await Relationship.find({
-          $or: [
-            { fromUserID: userID, status: "pending" },
-            { toUserID: userID, status: "pending" },
-          ]
-        });
-        return relationships;
-      default:
-        return [];
-    }
-  }
-  catch (error) {
-    return error;
-  }
-}
+// /**
+//  * Get pending relationships for a user.
+//  * 
+//  * Useful for getting a list of users that:
+//  * - have sent a friend request to a user
+//  * - a user has sent a friend request to
+//  * ... but the request has not been accepted yet.
+//  */
+// export const getPending = async (userID: String, direction: "outgoing" | "incoming" | "all") => {
+//   try {
+//     let relationships;
+//     switch (direction) {
+//       case "outgoing":
+//         relationships = await Relationship.find({ fromUserID: userID, status: "pending" });
+//         return relationships;
+//       case "incoming":
+//         relationships = await Relationship.find({ toUserID: userID, status: "pending" });
+//         return relationships;
+//       case "all":
+//         relationships = await Relationship.find({
+//           $or: [
+//             { fromUserID: userID, status: "pending" },
+//             { toUserID: userID, status: "pending" },
+//           ]
+//         });
+//         return relationships;
+//       default:
+//         return [];
+//     }
+//   }
+//   catch (error) {
+//     return error;
+//   }
+// }
 
-/**
- * Get all *user IDs* for a user that match a given status.
- * 
- * @returns {Array} - An array of user IDs that match the given status
- *   in a relationship with current user ID.
- */
-export const getConnections = async (userID: String, status: "pending" | "accepted" | "blocked" | "declined" | "all") => {
-  try {
-    let relationships;
-    if (status == "all") {
-      relationships = await Relationship.find({
-        $and: [
-          { $or: [
-            { fromUserID: userID },
-            { toUserID: userID },
-          ]},
-        ]
-      });
-    }
-    else {
-      relationships = await Relationship.find({
-        $and: [
-          { $or: [
-            { fromUserID: userID },
-            { toUserID: userID },
-          ]},
-          { status },
-        ]
-      });
-    }
+// /**
+//  * Get all *user IDs* for a user that match a given status.
+//  * 
+//  * @returns {Array} - An array of user IDs that match the given status
+//  *   in a relationship with current user ID.
+//  */
+// export const getConnections = async (userID: String, status: "pending" | "accepted" | "blocked" | "declined" | "all") => {
+//   try {
+//     let relationships;
+//     if (status == "all") {
+//       relationships = await Relationship.find({
+//         $and: [
+//           { $or: [
+//             { fromUserID: userID },
+//             { toUserID: userID },
+//           ]},
+//         ]
+//       });
+//     }
+//     else {
+//       relationships = await Relationship.find({
+//         $and: [
+//           { $or: [
+//             { fromUserID: userID },
+//             { toUserID: userID },
+//           ]},
+//           { status },
+//         ]
+//       });
+//     }
     
-    // parse relationships to get a list of user IDs that are not current userID
-    const connections = relationships.map((relationship) => {
-      if (relationship.fromUserID.toString() === userID) {
-        return relationship.toUserID;
-      } else {
-        return relationship.fromUserID;
-      }
-    });
+//     // parse relationships to get a list of user IDs that are not current userID
+//     const connections = relationships.map((relationship) => {
+//       if (relationship.fromUserID.toString() === userID) {
+//         return relationship.toUserID;
+//       } else {
+//         return relationship.fromUserID;
+//       }
+//     });
 
-    return connections
-  } catch (error) {
-    return error;
-  }
-}
+//     return connections
+//   } catch (error) {
+//     return error;
+//   }
+// }
--- src/router.ts
+++ src/router.ts
@@ -49,6 +49,20 @@ router.get('/users/:id', requireAuth, async (req, res) => {
   }
 });
 
+router.post('/relationships', requireAuth, async (req, res) => {
+
+  // get the fromUserID from the request body
+  const fromUserID = req.user._id;
+  const toUserID = req.body.toUserID;
+  try {
+    const relationship = await Relationships.create({ fromUserID, toUserID });
+    res.json(relationship);
+  } catch (error) {
+    // res.status(422).send({ error: "hello, world" });
+    res.json({ error: error.toString() });
+  }
+})
+
 /**
  * Get all relationships for a user.
  * 
@@ -57,89 +71,110 @@ router.get('/users/:id', requireAuth, async (req, res) => {
  * Caller should handle filtering or use one of the other endpoints
  * that promise to return a specific subset of relationships.
  */
-router.get('/relationships/all/:userId', requireAuth, async (req, res) => {
-  try {
-    const relationships = await Relationships.getAll(req.params.userId);
-    res.json(relationships);
-  } catch (error) {
-    res.status(422).send({ error: error.toString() });
-  }
-});
+router.get('/relationships/:userId', requireAuth, async (req, res) => {
+  
+  // get direction from query params
+  const direction = req.query.direction || 'all';
+  
+  // get status from query params
+  const status: string = req.query.status.toString() || 'all';
+  
+  let relationships;
 
-/**
- * Get outgoing relationships for a user
- * 
- * Useful for getting a list of users that a user has sent a friend request to.
- */
-router.get('/relationships/outgoing/:userId', requireAuth, async (req, res) => {
   try {
-    const relationships = await Relationships.getOutgoing(req.params.userId);
-    res.json(relationships);
-  } catch (error) {
-    res.status(422).send({ error: error.toString() });
-  }
-});
-
-/**
- * Get incoming relationships for a user.
- * 
- * Useful for getting a list of users that have sent a friend request to a user.
- */
-router.get('/relationships/incoming/:userId', requireAuth, async (req, res) => {
-  try {
-    const relationships = await Relationships.getIncoming(req.params.userId);
-    res.json(relationships);
-  } catch (error) {
-    res.status(422).send({ error: error.toString() });
-  }
-});
-
-/**
- * Get pending relationships for a user.
- * 
- * Useful for getting a list of users that:
- *  - have sent a friend request to a user
- *  - a user has sent a friend request to
- * ... but the request has not been accepted yet.
- * 
- * @param direction - "outgoing" | "incoming" | "all"
- */
-router.get('/relationships/pending/:userId/:direction', requireAuth, async (req, res) => {
-  try {
-    // get direction from req.params.direction and make sure it is valid
-    const direction = req.params.direction.toLowerCase();
-    if (direction !== 'outgoing' && direction !== 'incoming' && direction !== 'all') {
-      throw new Error('Invalid direction');
+    switch (direction) {
+      case 'outgoing':
+        relationships = await Relationships.getOutgoing(req.params.userId, status);
+        break;
+      case 'incoming':
+        relationships = await Relationships.getIncoming(req.params.userId, status);
+        break;
+      case 'all':
+        relationships = await Relationships.getAll(req.params.userId, status);
+        break;
+      default:
+        relationships = [];
     }
-    
-    const relationships = await Relationships.getPending(req.params.userId, direction);
     res.json(relationships);
   } catch (error) {
     res.status(422).send({ error: error.toString() });
   }
 });
 
-/**
- * Get connections for a user.
- * 
- * Parses the relationships for given user and returns a list of **other**
- * users that have a relationship with the given user.
- * 
- * Filters 
- */
-router.get('/relationships/connections/:userId/:status', requireAuth, async (req, res) => {
-  try {
-    // get status from req.params.status and make sure it is valid
-    const status = req.params.status.toLowerCase();
-    if (status !== 'accepted' && status !== 'declined' && status !== 'blocked' && status !== 'pending' && status !== 'all') {
-      throw new Error('Invalid status');
-    }
+// /**
+//  * Get outgoing relationships for a user
+//  * 
+//  * Useful for getting a list of users that a user has sent a friend request to.
+//  */
+// router.get('/relationships/outgoing/:userId', requireAuth, async (req, res) => {
+//   try {
+//     const relationships = await Relationships.getOutgoing(req.params.userId);
+//     res.json(relationships);
+//   } catch (error) {
+//     res.status(422).send({ error: error.toString() });
+//   }
+// });
+
+// /**
+//  * Get incoming relationships for a user.
+//  * 
+//  * Useful for getting a list of users that have sent a friend request to a user.
+//  */
+// router.get('/relationships/incoming/:userId', requireAuth, async (req, res) => {
+//   try {
+//     const relationships = await Relationships.getIncoming(req.params.userId);
+//     res.json(relationships);
+//   } catch (error) {
+//     res.status(422).send({ error: error.toString() });
+//   }
+// });
+
+// /**
+//  * Get pending relationships for a user.
+//  * 
+//  * Useful for getting a list of users that:
+//  *  - have sent a friend request to a user
+//  *  - a user has sent a friend request to
+//  * ... but the request has not been accepted yet.
+//  * 
+//  * @param direction - "outgoing" | "incoming" | "all"
+//  */
+// router.get('/relationships/pending/:userId/:direction', requireAuth, async (req, res) => {
+//   try {
+//     // get direction from req.params.direction and make sure it is valid
+//     const direction = req.params.direction.toLowerCase();
+//     if (direction !== 'outgoing' && direction !== 'incoming' && direction !== 'all') {
+//       throw new Error('Invalid direction');
+//     }
     
-    const relationships = await Relationships.getConnections(req.params.userId, status);
-    res.json(relationships);
-  } catch (error) {
-    res.status(422).send({ error: error.toString() });
-  }
-});
+//     const relationships = await Relationships.getPending(req.params.userId, direction);
+//     res.json(relationships);
+//   } catch (error) {
+//     res.status(422).send({ error: error.toString() });
+//   }
+// });
+
+// /**
+//  * Get connections for a user.
+//  * 
+//  * Parses the relationships for given user and returns a list of **other**
+//  * users that have a relationship with the given user.
+//  * 
+//  * Filters 
+//  */
+// router.get('/relationships/connections/:userId/:status', requireAuth, async (req, res) => {
+//   try {
+//     // get status from req.params.status and make sure it is valid
+//     const status = req.params.status.toLowerCase();
+//     if (status !== 'accepted' && status !== 'declined' && status !== 'blocked' && status !== 'pending' && status !== 'all') {
+//       throw new Error('Invalid status');
+//     }
+    
+//     const relationships = await Relationships.getConnections(req.params.userId, status);
+//     res.json(relationships);
+//   } catch (error) {
+//     res.status(422).send({ error: error.toString() });
+//   }
+// });
 
 export default router;
