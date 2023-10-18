4110fca0df78b9cbb2d07bb9dcf0f0ecb99f871a
Kevin Lin <linkevin281@gmail.com>
2023-10-18 00:28:55 -0400

    Add logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import cors from 'cors'
-import apiRoutes from './router';
-import mongoose from 'mongoose';
-
-require('dotenv').config();
+import apiRoutes from './router'
+import mongoose from 'mongoose'
+import { logger, requestLogger, responseLogger } from './services/logger'
 
 const app = express()
 const port: Number = 3000
@@ -13,20 +12,20 @@ app.use(cors())
 app.options('*', cors())
 
 app.use(express.json())
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+app.use(requestLogger)
+app.use(responseLogger)
 
 async function runApp() {
-    try {
-        const mongoURI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/short-form-api';
-        await mongoose.connect(mongoURI);
-        await app.listen(port)
-        console.log(`Server listening at http://localhost:${port}`)
-    } catch (err) {
-        console.error(err)
-    } 
+  try {
+    const mongoURI =
+      process.env.MONGODB_URI
+    await mongoose.connect(mongoURI)
+    await app.listen(port)
+    console.log(`Server listening at http://localhost:${port}`)
+  } catch (err) {
+    logger.error(err)
+    console.error(err)
+  }
 }
 
 app.use('/api', apiRoutes)
