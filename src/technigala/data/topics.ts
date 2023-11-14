af40bc809b777fcf922fdc9db8be98c878357198
Colton Sankey <csank@Kevins-Mac-mini.local>
2023-11-14 03:41:31 -0500

    updated topics for technigala

--- src/technigala/data/topics.ts
+++ src/technigala/data/topics.ts
@@ -3,117 +3,163 @@ export interface PopTopicsDocument {
   topicName: string
   subTopicName: string
   thumbnailurl: string
+  displayTopicName: string
+  displaySubtopicName: string
 }
 
 export const topicDocuments: PopTopicsDocument[] = [
   {
     topicName: 'coffee',
     subTopicName: 'coffee-workflow',
-    thumbnailurl: 'coffeeurl'
+    thumbnailurl: 'coffeeurl',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Coffee WorkFlow"
   },
   {
     topicName: 'coffee',
     subTopicName: 'coffee-research',
-    thumbnailurl: 'coffeeurl'
+    thumbnailurl: 'coffeeurl',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Coffee Research"
   },
   {
     topicName: 'coffee',
     subTopicName: 'latte-art',
-    thumbnailurl: 'coffeeurl'
+    thumbnailurl: 'coffeeurl',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Latte Art"
   },
   {
     topicName: 'coffee',
     subTopicName: 'brewing-techniques',
-    thumbnailurl: 'coffeeurl'
+    thumbnailurl: 'coffeeurl',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Brewing Techniques"
   },
   {
     topicName: 'home-design',
     subTopicName: 'kitchen-design',
-    thumbnailurl: 'homeurl'
+    thumbnailurl: 'homeurl',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Kitchen Design"
   },
   {
     topicName: 'home-design',
     subTopicName: 'minimalism',
-    thumbnailurl: 'homeurl'
+    thumbnailurl: 'homeurl',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Minimalism"
   },
   {
     topicName: 'home-design',
     subTopicName: 'landscaping',
-    thumbnailurl: 'homeurl'
+    thumbnailurl: 'homeurl',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Landscaping"
   },
   {
     topicName: 'home-design',
     subTopicName: 'sustainable-design',
-    thumbnailurl: 'homeurl'
+    thumbnailurl: 'homeurl',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Sustainable Design"
   },
   {
     topicName: 'home-design',
     subTopicName: 'tiny-homes',
-    thumbnailurl: 'homeurl'
+    thumbnailurl: 'homeurl',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Tiny Homes"
   },
   {
     topicName: 'second-punic-war',
     subTopicName: 'crossing-the-alps',
-    thumbnailurl: 'punicurl'
+    thumbnailurl: 'punicurl',
+    displayTopicName: "Second Punic War",
+    displaySubtopicName: "Crossing the Alps"
   },
   {
     topicName: 'second-punic-war',
     subTopicName: 'lake-trasimene',
-    thumbnailurl: 'punicurl'
+    thumbnailurl: 'punicurl',
+    displayTopicName: "Second Punic War",
+    displaySubtopicName: "Lake Trasimene"
   },
   {
     topicName: 'second-punic-war',
     subTopicName: 'cannae',
-    thumbnailurl: 'punicurl'
+    thumbnailurl: 'punicurl',
+    displayTopicName: "Second Punic War",
+    displaySubtopicName: "Cannae"
   },
   {
     topicName: 'second-punic-war',
     subTopicName: 'zama',
-    thumbnailurl: 'punicurl'
+    thumbnailurl: 'punicurl',
+    displayTopicName: "Second Punic War",
+    displaySubtopicName: "Zama"
   },
   {
     topicName: 'mathematics',
     subTopicName: 'calculus',
-    thumbnailurl: 'mathurl'
+    thumbnailurl: 'mathurl',
+    displayTopicName: "Mathematics",
+    displaySubtopicName: "Calculus"
   },
   {
     topicName: 'mathematics',
     subTopicName: 'algebra',
-    thumbnailurl: 'mathurl'
+    thumbnailurl: 'mathurl',
+    displayTopicName: "Mathematics",
+    displaySubtopicName: "Algebra"
   },
   {
     topicName: 'mathematics',
     subTopicName: 'logic',
-    thumbnailurl: 'mathurl'
+    thumbnailurl: 'mathurl',
+    displayTopicName: "Mathematics",
+    displaySubtopicName: "Logic"
   },
   {
     topicName: 'arts-and-crafts',
     subTopicName: 'crocheting',
-    thumbnailurl: 'arturl'
+    thumbnailurl: 'arturl',
+    displayTopicName: "Arts & Crafts",
+    displaySubtopicName: "Crocheting"
   },
   {
     topicName: 'arts-and-crafts',
     subTopicName: 'origami',
-    thumbnailurl: 'arturl'
+    thumbnailurl: 'arturl',
+    displayTopicName: "Arts & Crafts",
+    displaySubtopicName: "Origami"
   },
   {
     topicName: 'arts-and-crafts',
     subTopicName: 'embroidery',
-    thumbnailurl: 'arturl'
+    thumbnailurl: 'arturl',
+    displayTopicName: "Arts & Crafts",
+    displaySubtopicName: "Embroidery"
   },
   {
     topicName: 'cars',
     subTopicName: 'engines',
-    thumbnailurl: 'carurl'
+    thumbnailurl: 'carurl',
+    displayTopicName: "Cars",
+    displaySubtopicName: "Engines"
   },
   {
     topicName: 'cars',
     subTopicName: 'maintenance',
-    thumbnailurl: 'carurl'
+    thumbnailurl: 'carurl',
+    displayTopicName: "Cars",
+    displaySubtopicName: "Maintenance"
   },
   {
     topicName: 'cars',
     subTopicName: 'snow-driving-tips',
-    thumbnailurl: 'carurl'
+    thumbnailurl: 'carurl',
+    displayTopicName: "Cars",
+    displaySubtopicName: "Snow Driving Tips"
   }
 ]
