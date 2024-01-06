eb1dd3776e300114a0d82c60ce4ce62200a39d88
Kevin Lin <linkevin281@gmail.com>
2024-01-06 16:19:39 -0500

    Janky scaffolding DB deploy code and ML integration for Technigala

---
+++ src/technigala/data/topics.ts
@@ -0,0 +1,130 @@
+
+export interface PopTopicsDocument {
+  topicName: string
+  subTopicName: string
+  thumbnailurl: string
+  displayTopicName: string
+  displaySubtopicName: string
+}
+
+export const topicDocuments: PopTopicsDocument[] = [
+  {
+    topicName: 'coffee',
+    subTopicName: 'coffee-workflow',
+    thumbnailurl: 'cup.and.saucer.fill',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Coffee WorkFlow"
+  },
+  {
+    topicName: 'coffee',
+    subTopicName: 'coffee-research',
+    thumbnailurl: 'cup.and.saucer.fill',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Coffee Research"
+  },
+  {
+    topicName: 'coffee',
+    subTopicName: 'latte-art',
+    thumbnailurl: 'cup.and.saucer.fill',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Latte Art"
+  },
+  {
+    topicName: 'coffee',
+    subTopicName: 'brewing-techniques',
+    thumbnailurl: 'cup.and.saucer.fill',
+    displayTopicName: "Coffee",
+    displaySubtopicName: "Brewing Techniques"
+  },
+  {
+    topicName: 'home-design',
+    subTopicName: 'kitchen-design',
+    thumbnailurl: 'hammer.fill',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Kitchen Design"
+  },
+  {
+    topicName: 'home-design',
+    subTopicName: 'minimalism',
+    thumbnailurl: 'hammer.fill',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Minimalism"
+  },
+  {
+    topicName: 'home-design',
+    subTopicName: 'landscaping',
+    thumbnailurl: 'hammer.fill',
+    displayTopicName: "Home Design",
+    displaySubtopicName: "Landscaping"
+  },
+  {
+    topicName: 'second-punic-war',
+    subTopicName: 'crossing-the-alps',
+    thumbnailurl: 'shield.lefthalf.filled',
+    displayTopicName: "Second Punic War",
+    displaySubtopicName: "Crossing the Alps"
+  },
+  {
+    topicName: 'mathematics',
+    subTopicName: 'calculus',
+    thumbnailurl: 'plusminus',
+    displayTopicName: "Mathematics",
+    displaySubtopicName: "Calculus"
+  },
+  {
+    topicName: 'mathematics',
+    subTopicName: 'algebra',
+    thumbnailurl: 'plusminus',
+    displayTopicName: "Mathematics",
+    displaySubtopicName: "Algebra"
+  },
+  {
+    topicName: 'mathematics',
+    subTopicName: 'logic',
+    thumbnailurl: 'plusminus',
+    displayTopicName: "Mathematics",
+    displaySubtopicName: "Logic"
+  },
+  {
+    topicName: 'arts-and-crafts',
+    subTopicName: 'crocheting',
+    thumbnailurl: 'paintbrush.pointed.fill',
+    displayTopicName: "Arts & Crafts",
+    displaySubtopicName: "Crocheting"
+  },
+  {
+    topicName: 'arts-and-crafts',
+    subTopicName: 'origami',
+    thumbnailurl: 'paintbrush.pointed.fill',
+    displayTopicName: "Arts & Crafts",
+    displaySubtopicName: "Origami"
+  },
+  {
+    topicName: 'arts-and-crafts',
+    subTopicName: 'embroidery',
+    thumbnailurl: 'paintbrush.pointed.fill',
+    displayTopicName: "Arts & Crafts",
+    displaySubtopicName: "Embroidery"
+  },
+  {
+    topicName: 'cars',
+    subTopicName: 'engines',
+    thumbnailurl: 'car.fill',
+    displayTopicName: "Cars",
+    displaySubtopicName: "Engines"
+  },
+  {
+    topicName: 'cars',
+    subTopicName: 'maintenance',
+    thumbnailurl: 'car.fill',
+    displayTopicName: "Cars",
+    displaySubtopicName: "Maintenance"
+  },
+  {
+    topicName: 'cars',
+    subTopicName: 'snow-driving-tips',
+    thumbnailurl: 'car.fill',
+    displayTopicName: "Cars",
+    displaySubtopicName: "Snow Driving Tips"
+  }
+]
---
+++ src/technigala/utils.ts
@@ -0,0 +1,36 @@
+import fs from 'fs'
+
+export const createFile = async (name: string, theMap: Map<string, string>) => {
+  // create a file called ${name}.ts
+
+  fs.writeFile(
+    `./src/technigala/maps/${name}.ts`,
+    `export const ${name}Map = {
+  ${Array.from(theMap).map(([key, value]) => `  "${key}": "${value}"`)})`,
+    (err) => {
+      if (err) {
+        console.log(err)
+      }
+    }
+  )
+}
+
+export const createTopicVideoFile = async (
+  name: string,
+  theMap: Map<string, string[]>
+) => {
+  // create a file called ${name}.ts
+
+  fs.writeFile(
+    `./src/utils/${name}.ts`,
+    `export const ${name}Map = {
+  ${Array.from(theMap).map(
+    ([key, value]) => `  "${key}": ${JSON.stringify(value)}`
+  )}}`,
+    (err) => {
+      if (err) {
+        console.log(err)
+      }
+    }
+  )
+}
