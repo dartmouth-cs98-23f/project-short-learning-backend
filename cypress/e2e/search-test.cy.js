ec70761e669a4916c2d6c28ceeca992982f9d8eb
siavava <amittaijoel@outlook.com>
2024-02-25 23:45:09 -0500

    integrated routes + unit test

---
+++ cypress/e2e/search-test.cy.js
@@ -0,0 +1,33 @@
+const URL = 'localhost:3000/api'
+
+describe('Search API Tests', () => {
+  it('should return search results for topics', () => {
+    cy.request({
+      method: 'GET',
+      url: `${URL}/search/topics?q=java`,
+      // headers: {
+      //   Authorization: token
+      // }
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('results');
+        expect(response.body.results).to.be.an('array');
+        expect(response.body.results.length).to.be.greaterThan(0);
+      });
+  });
+
+  it('should return search results for video transcripts', () => {
+    cy.request({
+      method: 'GET',
+      url: `${URL}/search/videos?q=rust`,
+      // headers: {
+      //   Authorization: token
+      // }
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('results');
+        expect(response.body.results).to.be.an('array');
+        expect(response.body.results.length).to.be.greaterThan(0);
+      });
+  });
+});
