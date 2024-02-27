f6781bd8d2e035ed2a8e31488dfa3557b3296db0
siavava <amittaijoel@outlook.com>
2024-02-27 14:10:08 -0500

    added unit tests

--- src/routes/vectorized-recommendations-routes.ts
+++ src/routes/vectorized-recommendations-routes.ts
@@ -8,8 +8,8 @@ const vectorizedRecRouter = Router()
 /**
  * GET /vectorized-recommendations
  * 
- * @param {string} videoId - the videoId of the video to get recommendations from
- * @param {string} userId - the userId of the user to get recommendations for
+ * @bodyparam {string} videoId - the videoId of the video to get recommendations from
+ * @bodyparam {string} userId - the userId of the user to get recommendations for
  */
 vectorizedRecRouter.get('/vectorized-recommendations', requireAdmin, async (req: Request, res: Response) => {
   const videoId = req.query?.videoId?.toString() || ""
