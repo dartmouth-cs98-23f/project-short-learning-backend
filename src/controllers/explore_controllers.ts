c168f8007128eb2a83ec328c08e0a160047508aa
Kevin Lin <linkevin281@gmail.com>
2024-03-04 02:02:39 -0500

    Explore topic page and general explore page

---
+++ src/controllers/explore_controllers.ts
@@ -0,0 +1,260 @@
+/**
+ * Controllers for search endpoints
+ */
+
+import { logger } from '../services/logger'
+import {
+  TopicResult,
+  RankedSearchResults,
+  VideoResult
+} from '../models/explore_models'
+import algoliasearch from 'algoliasearch'
+import { VideoMetadata } from '../models/video_models'
+import UserModel from '../models/user_model'
+import { Pinecone } from '@pinecone-database/pinecone'
+import { roleAffinities } from '../utils/roles'
+import { indexedMap, reverseIndexedMap } from '../utils/topics'
+import UserAffinityModel from '../models/user_affinity_model'
+
+const PAGE_SIZE = 8
+
+const appID = process.env.ALGOLIA_SEARCH_APP_ID
+const apiKey = process.env.ALGOLIA_SEARCH_API_KEY
+const indexName = process.env.ALGOLIA_SEARCH_INDEX_NAME
+const topicsIndexName = process.env.ALGOLIA_SEARCH_TOPICS_INDEX_NAME
+const PINECONE_API_KEY = process.env.PINECONE_API_KEY
+const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME
+const PINECONE_INDEX_NAME_TOPICS = process.env.PINECONE_INDEX_NAME_TOPICS
+
+const client = algoliasearch(appID, apiKey)
+const index = client.initIndex(indexName)
+const topicsIndex = client.initIndex(topicsIndexName)
+
+const pineconeClient = new Pinecone({
+  apiKey: PINECONE_API_KEY
+})
+
+export const searchTranscript = async (
+  query: string
+): Promise<RankedSearchResults> => {
+  if (!query) {
+    return { videos: [], topics: [] }
+  }
+
+  try {
+    return index.search(query).then(async (res) => {
+      let topics = await accumulateTopics(res.hits)
+      let videos = await accumulateVideos(res.hits)
+
+      return { videos, topics: topics.slice(0, 10) }
+    })
+  } catch (error) {
+    logger.error(error)
+    throw new Error(`SEARCH ERROR: ${error}`)
+  }
+}
+
+export const searchTopics = async (
+  query: string
+): Promise<RankedSearchResults> => {
+  if (!query) {
+    return { videos: [], topics: [] }
+  }
+
+  try {
+    return topicsIndex.search(query).then(async (res) => {
+      const topics = await accumulateTopics(res.hits)
+      const videos = await accumulateVideos(res.hits)
+
+      return { videos, topics: topics.slice(0, 10) }
+    })
+  } catch (error) {
+    logger.error(error)
+    throw new Error(`SEARCH ERROR: ${error}`)
+  }
+}
+
+export const searchUser = async (query: string) => {
+  if (!query) {
+    return { users: [] }
+  }
+
+  try {
+    // search for user in user model (firs name, last name, username, email)
+    const users = await UserModel.find({
+      $or: [
+        { firstName: { $regex: query, $options: 'i' } },
+        { lastName: { $regex: query, $options: 'i' } },
+        { username: { $regex: query, $options: 'i' } },
+        { email: { $regex: query, $options: 'i' } }
+      ]
+    })
+
+    // filter out sensitive information
+    return users.map((user) => {
+      return {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        email: user.email
+      }
+    })
+  } catch (error) {
+    logger.error(error)
+    throw new Error(`SEARCH ERROR: ${error}`)
+  }
+}
+
+export const searchAll = async (query) => {
+  try {
+    const queries = [
+      searchTranscript(query.q),
+      searchTopics(query.topic),
+      searchUser(query.user)
+    ]
+
+    const results: any = await Promise.all(queries)
+
+    const videos = results[0].videos.concat(results[1].videos)
+    const topics = results[0].topics.concat(results[1].topics)
+    const users = results[2]
+
+    return { videos, topics, users }
+  } catch (error) {
+    logger.error(error)
+    throw new Error(`SEARCH ERROR: ${error}`)
+  }
+}
+
+function accumulateTopics(results: any): TopicResult[] {
+  let _topics = new Map<string, number>()
+
+  results.forEach((result: any) => {
+    result.topics.forEach((topic: any) => {
+      _topics[topic] = _topics[topic] || 0
+      _topics[topic] += 1
+    })
+  })
+
+  let sortedArray = []
+  for (let k in _topics) {
+    const v = _topics[k]
+    sortedArray.push({ k, v })
+  }
+
+  sortedArray.sort((a, b) => b.v - a.v)
+
+  let topics: TopicResult[] = sortedArray.map((topic) => {
+    return {
+      topic: topic['k'],
+      score: topic['v']
+    }
+  })
+  return topics
+}
+
+async function accumulateVideos(results: any): Promise<VideoResult[]> {
+  const res = await Promise.all(
+    results.map(async (hit: any) => {
+      return await VideoMetadata.findById(hit.objectID)
+        .populate('clips')
+        .exec()
+        .then((metadata) => {
+          return {
+            videoId: hit.objectID,
+            // highlight: hit._highlightResult,
+            topics: hit?.topics,
+            title: hit?.title,
+            description: hit?.description,
+            metadata: metadata
+          }
+        })
+    })
+  )
+  return res
+}
+
+export const getTopicRankings = async (userId: string) => {
+  try {
+    const roles = Object.keys(roleAffinities)
+    const userAffinityDoc = await UserAffinityModel.findOne({
+      userId: userId
+    })
+    const userAffinities = userAffinityDoc.affinities
+    const rankings: { [key: string]: number } = {}
+    roles.forEach((role) => {
+      rankings[role] = 0
+    })
+
+    Object.entries(roleAffinities).forEach(([role, affinities]) => {
+      Object.keys(indexedMap).forEach((topic) => {
+        rankings[role] += affinities[topic] * userAffinities.get(topic)
+      })
+      logger.debug(`role: ${role}, ranking: ${rankings[role]}`)
+    })
+    const sorted = Object.keys(rankings).sort(
+      (a, b) => rankings[b] - rankings[a]
+    )
+    return sorted
+  } catch (error) {
+    logger.error(error)
+  }
+}
+
+// To get topic to return, get their {page} % 6 best topic
+export const getExplore = async (userId: string, page: number) => {
+  try {
+    const topic = getTopicRankings(userId)[(page - 1) % 6]
+  } catch (error) {
+    logger.error(error)
+  }
+}
+// TODO: CACHING AND USER HISTORY
+export const getTopicExplore = async (
+  userId: string,
+  topicId: string,
+  page: number
+) => {
+  try {
+    const topicIndex = pineconeClient.index(PINECONE_INDEX_NAME_TOPICS)
+    const index = pineconeClient.index(PINECONE_INDEX_NAME)
+
+    const avgVectorId = `${topicId}_avg`
+    const response = await topicIndex.fetch([avgVectorId])
+    const avgTopicRecord = response.records[avgVectorId]
+
+    const searchResponse = await index.query({
+      vector: avgTopicRecord.values,
+      topK: PAGE_SIZE * page,
+      includeMetadata: true,
+      filter: { topics: { $in: [topicId] } }
+    })
+
+    const videos = await Promise.all(
+      searchResponse.matches.map(async (match) => {
+        const videoId = match.id.slice(0, -4)
+        const metadata = await VideoMetadata.findById(videoId)
+          .populate('clips')
+          .exec()
+        return metadata
+      })
+    )
+    return videos.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE)
+  } catch (error) {
+    logger.error(error)
+  }
+}
+
+export const getTopicGraphValues = async (topicId: string) => {
+  try {
+    const graphValues: number[] = []
+    const convertedTopicId = reverseIndexedMap[topicId]
+    Object.entries(roleAffinities).forEach(([_, affinities]) => {
+      graphValues.push(affinities[convertedTopicId])
+    })
+    return graphValues
+  } catch (error) {
+    logger.error(error)
+  }
+}
---
+++ src/routes/explore_routes.ts
@@ -0,0 +1,116 @@
+import { Request, Response, Router } from 'express'
+import { requireAdmin, requireAuth } from '../services/passport'
+import { logger } from '../services/logger'
+import * as ExploreController from '../controllers/explore_controllers'
+
+const searchRouter = Router()
+
+/**
+ * GET /explore/search/videos
+ *
+ * @bodyparam {string} q - the search query
+ * @bodyparam {string} topic - the topic to search for
+ *
+ * NOTE: It is okay to provide one or the other, but at least one must be provided.
+ *
+ * if both are provided, the query will be used.
+ */
+searchRouter.get(
+  '/explore/search',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const query = req.query.q?.toString()
+    const topic = req.query.topic?.toString()
+    const user = req.query.user?.toString()
+    const all = req.query.all?.toString()
+
+    try {
+      let results
+
+      if (all) {
+        results = await ExploreController.searchAll({
+          q: query,
+          topic: topic,
+          user: user
+        })
+      } else if (query) {
+        results = await ExploreController.searchTranscript(query)
+      } else if (topic) {
+        results = await ExploreController.searchTopics(topic)
+      } else if (user) {
+        results = await ExploreController.searchUser(user)
+        return res.status(200).json({ users: results })
+      }
+
+      // if results is set, return it
+      if (results) {
+        return res.status(200).json(results)
+      } else {
+        return res
+          .status(400)
+          .json({ error: 'No query, topic, or user regex provided' })
+      }
+    } catch (error) {
+      logger.error(error)
+      return res.status(500).json({ error: error })
+    }
+  }
+)
+
+/**
+ * GET /explore/explorepage
+ *
+ *
+ * Used for the explore page.
+ */
+searchRouter.get(
+  '/explore/explorepage',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    try {
+      const userId = req.user.id
+      const page = req.body.page || 1
+      const results = await ExploreController.getExplore(userId, page)
+      return res.status(200).json({ ranking: results })
+    } catch (error) {
+      logger.error(error)
+      return res.status(500).json({ error: error })
+    }
+  }
+)
+
+/**
+ * GET /explore/topicpage/:topic
+ *
+ * @pathparam {string} topic - the topic to search for
+ * @bodyparam {number} page - the page number to get
+ *
+ * Used to get the explore page of a specific topic.
+ *
+ * Returns:
+ *  - videos {<VideoMetadata>[]}: the videos for the topic
+ *  - graphValues {number[]}: the graph values for the topic
+ */
+searchRouter.get(
+  '/explore/topicpage/:topicId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    try {
+      const topicId = req.params.topicId
+      const page = req.body.page || 1
+      const userId = req.user.id
+      const results = await ExploreController.getTopicExplore(
+        userId,
+        topicId,
+        page
+      )
+      const graphValues = await ExploreController.getTopicGraphValues(topicId)
+      return res.status(200).json({ videos: results, graphValues })
+    } catch (error) {
+      logger.error(error)
+      return res.status(500).json({ error: error })
+    }
+  }
+)
+
+export default searchRouter
