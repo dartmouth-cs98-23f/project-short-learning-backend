3b289f7e99aef7a1a571f4f1e73714d9deea08e7
Kevin Lin <linkevin281@gmail.com>
2024-03-04 15:47:39 -0500

    Add active affinities and active history objects

--- src/models/user_affinity_model.ts
+++ src/models/user_affinity_model.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from 'mongoose'
 
+const MAX_ACTIVE_AFFINITIES = 10
+
 export interface AffinityObject {
   affinities: Record<string, number>
   timestamp: number
@@ -11,17 +13,37 @@ export interface UserAffinityDocument extends mongoose.Document {
   activeAffinities: AffinityObject[]
 }
 
-const UserAffinitySchema = new Schema<UserAffinityDocument>(
+export interface AffinityObject {
+  affinities: Record<string, number>
+  timestamp: number
+}
+export interface UserAffinityDocument extends mongoose.Document {
+  userId: mongoose.Types.ObjectId
+  affinities: Map<string, number>
+  complexities: Map<string, number>
+  activeAffinities: AffinityObject[]
+}
+
+const UserAffinitySchema = new Schema<UserAffinityDocument><UserAffinityDocument>(
   {
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     affinities: { type: Map, of: Number, required: false },
     complexities: { type: Map, of: Number, required: false },
-    activeAffinities: [
-      {
-        affinities: { type: Map, of: Number, required: false },
-        timestamp: { type: Number, required: false }
+    activeAffinities: {
+      type: [
+        {
+          affinities: { type: Map, of: Number, required: true },
+          timestamp: { type: Number, required: true }
+        }
+      ],
+      required: false,
+      validate: {
+        validator: function (v: AffinityObject[]) {
+          return v.length <= MAX_ACTIVE_AFFINITIES
+        },
+        message: `Active affinities array cannot be longer than ${MAX_ACTIVE_AFFINITIES}`
       }
-    ]
+    }
   },
   {
     toObject: { virtuals: true },
